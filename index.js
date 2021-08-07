afbe4c0a78f4ecc3a5020caf06081f7faca167be
menomenonkwan <menomenonkwan@gmail.com>
2021-08-07 16:28:44 -0700

    added icon and currency api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const photographer = document.querySelector('.photograher');
 const time = document.querySelector('.current-time');
 const day = document.querySelector('.current-day');
-const photographer = document.querySelector('.photograher');
+const currency = document.querySelector('.currency');
 
 // GET BACKGROUND IMAGE from unsplash on page load
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
@@ -23,3 +24,22 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     time.textContent = calendar.toLocaleTimeString("en-us", {timeStyle: 'medium'});
     day.textContent = calendar.toLocaleString("en-us", options);
   }, 1000);
+
+
+  // GET CURRENCY INFO
+  fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json')
+    .then(res => {
+      if (!res.ok) { throw Error("Something went wrong") }
+      return res.json()
+    })
+    .then(data => {
+      currency.innerHTML = `
+        <h3><i class="fas fa-dollar-sign"></i> USD Exchange Rates</h3>
+        <ul>
+          <li>EUR: <i class="fas fa-euro-sign"></i> ${data.usd.eur}</li>
+          <li>JPY: <i class="fas fa-yen-sign"></i> ${data.usd.jpy}</li>
+          <li>GBP: <i class="fas fa-pound-sign"></i> ${data.usd.gbp}</li>
+        </ul>
+      `;
+    })
+    .catch(err => console.error(err));
